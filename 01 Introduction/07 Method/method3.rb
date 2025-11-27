2d762b935a5eb3d9190ed9fdfed0484a605482f4
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 20:16:14 +0700

    Add files via upload

---
+++ 01 Introduction/07 Method/method3.rb
@@ -0,0 +1,9 @@
+#! /usr/local/bin/ruby
+
+def sunshine
+	puts "The Sun Will Shine"
+	puts "The Flowers Will Bloom"
+	puts "And Life Will Be Good Again"
+end
+
+sunshine
---
+++ 01 Introduction/07 Method/method4.rb
@@ -0,0 +1,10 @@
+#! /usr/local/bin/ruby
+
+def data(a = "HOPE", b = "12345", c = "Dreams")
+	puts "Value : #{a}"
+	puts "Value : #{b}"
+	puts "Value : #{c}"
+end
+
+data
+data "Get Up", "STAND UP", "Dont Give Up"
