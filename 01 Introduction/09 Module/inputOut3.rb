9c8a33914e56720736c6277c465009d4d192a17c
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 20:17:31 +0700

    Add files via upload

---
+++ 01 Introduction/09 Module/inputOut3.rb
@@ -0,0 +1,12 @@
+#! /usr/local/bin/ruby
+
+data = IO.new(1)
+
+# Output
+data.putc "The Sun Will Shine"
+data.putc "The Flowers Will Bloom"
+data.putc "And Life Will Be Good Again"
+
+# Input
+# data.getc
+data.gets
