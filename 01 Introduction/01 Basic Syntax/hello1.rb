020df7c200f67efc396938e6962ee198f67eb227
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 18:10:47 +0700

    hello1.rb

--- 01 Introduction/01 Basic Syntax/hello1.rb
+++ 01 Introduction/01 Basic Syntax/hello1.rb
@@ -1,5 +1,4 @@
 puts "Hello Hello"    # -> with newline
 #puts "Sometimes, Some Of Your Greatest Pain Become Your Greatest Strength"    # -> with newline
 print "Hello Hello"   # -> without newline
-#print "Never Give Up On Your Dreams, No Matter How Painful And Difficult Your Journey"   # -> without newline
 #print "Get Up, Stand Up, Dont Give Up"   # -> without newline
