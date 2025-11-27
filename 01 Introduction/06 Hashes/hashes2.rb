b3e66197b417377fd08cb8cd135acb26ab2e2c44
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 20:15:57 +0700

    Add files via upload

---
+++ 01 Introduction/06 Hashes/hashes2.rb
@@ -0,0 +1,10 @@
+#! /usr/local/bin/ruby
+
+hashes = Hash["a" => 5, "b" => 10, "c" => 15, "d" => 20, "e" => 25, "f" = 30] 
+
+puts "#{hashes['a']}"
+puts "#{hashes['b']}"
+puts "#{hashes['c']}"
+puts "#{hashes['d']}"
+puts "#{hashes['e']}"
+puts "#{hashes['f']}"
