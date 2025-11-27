060030491232333881f261bd664a755ee9167645
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 20:16:32 +0700

    Add files via upload

---
+++ 01 Introduction/08 Class and Object/class2.rb
@@ -0,0 +1,14 @@
+#! /usr/local/bin/ruby
+
+# Class
+class Sunshine
+	def hello
+		puts "The Sun Will Shine"
+		puts "The Flowers Will Bloom"
+		puts "And Life Will Be Good Again"
+	end
+end
+
+# Create Object
+data = Sunshine.new
+puts data.hello
