dec09b02df3771e7e389b95474d13e023866f111
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-11-27 20:05:35 +0700

    systemCommand.rb

--- 01 Introduction/12 System Command/systemCommand.rb
+++ 01 Introduction/12 System Command/systemCommand.rb
@@ -1,4 +1,6 @@
 #! /usr/local/bin/ruby
 
 system "echo 'Hello'"
+#system "echo 'Never Give Up On Your Dreams'"
 system "echo", "Hello", "Hello"
+#system "echo", "Strong", "Brave", "Humble", "Badass"
