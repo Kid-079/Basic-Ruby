bf02475d82ae127c035e5daa145ead7b899d4503
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-02 11:21:52 +0700

    yieldStatement4.rb

--- 05 Methods/04 Yield Statement/yieldStatement4.rb
+++ 05 Methods/04 Yield Statement/yieldStatement4.rb
@@ -6,5 +6,6 @@ def string
 end
 
 string = "Hello"     # --> Block Given
+#string = "If I Cannot Do Great Things, I Can Do Small Things In A Great Way"     # --> Block Given
 
 puts string          # --> No Eror : Program Success
