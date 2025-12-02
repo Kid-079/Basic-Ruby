e6d3a14d4120d7a207ea247f5c49d4dae1d5cfe6
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-02 11:36:57 +0700

    Add files via upload

---
+++ 05 Methods/03 Return Values/returnStatement3.rb
@@ -0,0 +1,16 @@
+#! /usr/local/bin/ruby
+
+def value
+	a = 15
+	b = 25
+	c = 35
+	d = 45
+	e = 55
+return a, b, c, d, e
+end
+
+data = value
+
+print data
+puts "\n"
+puts data
---
+++ 05 Methods/03 Return Values/returnStatement4.rb
@@ -0,0 +1,27 @@
+#! /usr/local/bin/ruby
+
+def data1
+	a = 15
+return a
+end
+
+def data2
+	b = 25
+	c = 35
+	d = 45
+return b, c, d
+end
+
+def data3
+	e = 55
+	f = 65
+	g = 75
+	h = 85
+return e, f, g, h
+end
+
+value = data1
+
+print value
+puts "\n"
+puts value
