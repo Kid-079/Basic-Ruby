505585ef5efe2550e28f362e96bd899ea1d41b8e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-02 11:35:14 +0700

    size4.rb

--- 05 Methods/06 Variable Size/size4.rb
+++ 05 Methods/06 Variable Size/size4.rb
@@ -9,11 +9,13 @@ def array(names, number, *data)
 end
 
 names = "ABC"
+#names = "FIGHTING"
 puts names
 
 puts array(0,1)
 puts array(5, 2.5, 5.5)
 puts array(5, 2.5, "two", 50, 5.5)
+puts array(5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
 
 number = "1"
 if number != "0"
