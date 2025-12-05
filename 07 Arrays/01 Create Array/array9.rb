75c541359d2778c847f3aef9398f0ee56fa20118
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-05 22:52:24 +0700

    array9.rb

--- 07 Arrays/01 Create Array/array9.rb
+++ 07 Arrays/01 Create Array/array9.rb
@@ -1,12 +1,15 @@
 #! /usr/local/bin/ruby
 
 number1 = Range.new(0,5)
+#number1 = Range.new(0,10)
 puts "INCLUSIVE\n"
 puts number1
 puts number1.to_a
 
-puts "EXCLUSIVE\n"
+
 number2 = Range.new(0,5,true)
+#number2 = Range.new(0,10,true)
+puts "EXCLUSIVE\n"
 puts number2
 puts number2.to_a
 
