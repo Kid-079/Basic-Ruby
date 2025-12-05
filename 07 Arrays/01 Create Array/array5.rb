5610c304523aaede6577257f416b5bd95876b8b6
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-05 22:42:30 +0700

    array5.rb

--- 07 Arrays/01 Create Array/array5.rb
+++ 07 Arrays/01 Create Array/array5.rb
@@ -1,9 +1,9 @@
 #! /usr/local/bin/ruby
 
-name = Array("A" "B" "C")
-#name = Array("R" "E" "M" "P" "E" "Y" "E" "K")
-#name = Array("M" "O" "L" "E" "N")
-#name = Array("B" "A" "K" "W" "A" "N")
+names = Array("A" "B" "C")
+#names = Array("R" "E" "M" "P" "E" "Y" "E" "K")
+#names = Array("M" "O" "L" "E" "N")
+#names = Array("B" "A" "K" "W" "A" "N")
 
-print name
+print names
 print "\n"
