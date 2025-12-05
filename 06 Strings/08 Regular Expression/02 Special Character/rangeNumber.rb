c058722129abbfd0cba7a254b72173874334a775
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-05 18:45:51 +0700

    Delete 06 Strings/08 Regular Expression/02 Special Character/rangeNumber.rb

--- 06 Strings/08 Regular Expression/02 Special Character/rangeNumber.rb
+++
@@ -1,8 +0,0 @@
-#! /usr/local/bin/ruby
-
-def number(string)
-	string =~ /[0-9]/
-end
-
-puts number("age is 20")      # String Found, Return indeks  
-puts number("age is twenty")  # String Not Found
