2f709093eac6f9ab683c64735d8c4976aa1a52fc
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-05 23:33:13 +0700

    Add files via upload

---
+++ 07 Arrays/01 Create Array/accessElement9.rb
@@ -0,0 +1,26 @@
+#! /usr/local/bin/ruby
+
+array = ["perkedel", 30, :bakwan, [122,111,123,117,114,114,105,123]]
+
+print array
+print "\n"
+print array[3]
+print "\n"
+print array[3][3]
+print "\n"
+print array.first
+print "\n"
+print array.first 2
+print "\n"
+print array.last
+print "\n"
+print array[-1]
+print "\n"
+print array[0, 3]
+print "\n"
+print array[0..3]
+print "\n"
+print array[0, 1]
+print "\n"
+print array[0..2]
+print "\n"
