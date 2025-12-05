ab22ea9bc477d0835d558d2180bf1810c243be1d
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-05 23:00:55 +0700

    accessElement2.rb

--- 07 Arrays/02 Access Elements/accessElement2.rb
+++ 07 Arrays/02 Access Elements/accessElement2.rb
@@ -1,6 +1,9 @@
 #! /usr/local/bin/ruby
 
 array = ["A","B","C"]
+#array = ["P","E","R","K","E","D","E","L"]
+#array = ["B","A","K","W","A","N"]
+#array = ["R","I","S","O","L","L","E","S"]
 
 array.each do |font|
 	puts font
