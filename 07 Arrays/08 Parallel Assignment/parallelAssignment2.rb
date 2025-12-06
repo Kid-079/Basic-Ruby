907e2edcefb09088f90707a932ad3be6b0957dd5
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-06 18:38:18 +0700

    Add files via upload

---
+++ 07 Arrays/08 Parallel Assignment/parallelAssignment2.rb
@@ -0,0 +1,14 @@
+#! /usr/local/bin/ruby
+
+# Paralel Assignment
+a, b, c, d, e = "KETOPRAK", 2.5, 5.5, 10, "Bakwan"
+
+puts a
+puts b
+puts c
+puts d
+puts e
+
+
+# Paralel assignment -> a variable can assign values 
+#                       of different data types : string, float, integer
