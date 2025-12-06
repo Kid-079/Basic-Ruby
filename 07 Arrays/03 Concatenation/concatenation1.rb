9bc87140009d0715c85c50f5552f29dea2369a70
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-06 18:42:50 +0700

    Add files via upload

---
+++ 07 Arrays/03 Concatenation/concatenation1.rb
@@ -0,0 +1,9 @@
+#! /usr/local/bin/ruby
+
+a = %w[A B C D E]
+b = %w[1 2 3 4 5]
+
+array = a + b
+
+print array
+print "\n"
---
+++ 07 Arrays/03 Concatenation/concatenation3.rb
@@ -0,0 +1,9 @@
+#! /usr/local/bin/ruby
+
+a = %w[abc A B C]
+b = %w[1 2 3 4 5]
+
+array = a.concat(b)
+
+print array
+print "\n"
