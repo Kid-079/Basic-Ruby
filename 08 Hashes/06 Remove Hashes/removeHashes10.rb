d66098729164285d7c9bedd3e1032e2175bcb78b
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-07 15:52:57 +0700

    Add files via upload

---
+++ 08 Hashes/06 Remove Hashes/removeHashes10.rb
@@ -0,0 +1,9 @@
+#! /usr/local/bin/ruby
+
+string = {1 => "pastel", 2 => "7569846578", 3 => "Rengginang", 4 => "Emping", 5 => "AMPYANG", 6 => "7169848575", 7 => "79786869", 8 => "Melinjo", 9 => "bakwan", 10 => "75697485", 11 => "RISOLLES", 12 => "Rempeyek", 13 => "Bala-Bala", 14 => "83657185", 15 => "Kroket", 16 => "PERKEDEL", 17 => "Seblak", 18 => "766977806982", 19 => "CIRENG", 20 => "mendoan"}
+
+string.clear
+print string
+print "\n"
+print string.empty?
+print "\n"
