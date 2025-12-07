73f1f556633af0caee4c917848994e47229b7c68
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-07 16:29:43 +0700

    Add files via upload

---
+++ 10 Experience Ruby/02 Process XML/01 Create XML/createXml4.rb
@@ -0,0 +1,22 @@
+#! /usr/local/bin/ruby
+
+require 'rexml/document'
+include REXML
+
+data = Document.new
+
+data << XMLDecl.new("1.0", "UTF-8")
+
+data << Comment.new("This is XML file")
+
+data.add_element("name", {"lang" => "en"})
+
+first = data.root.add_element("first")
+last = data.root.add_element("last")
+
+first.text = "Coco"
+last.text = "Pandan"
+
+data.write($stdout, 0)
+print "\n"
+# puts data.write($stdout, 0)
