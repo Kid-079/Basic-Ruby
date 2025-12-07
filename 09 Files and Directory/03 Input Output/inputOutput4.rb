b03542a3dad0fcf8aa2c3866ef2d836aa16b8184
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-07 15:59:44 +0700

    inputOutput4.rb

--- 09 Files and Directory/03 Input Output/inputOutput4.rb
+++ 09 Files and Directory/03 Input Output/inputOutput4.rb
@@ -1,1 +1,10 @@
 #! /usr/local/bin/ruby
+
+data = IO.new(1, "w")
+
+data << "Keep Struggle" << "Keep Fighting" << "\n"
+
+data.flush       # Write String to the Stream
+puts data.flush
+
+data.close       # Close stream with close (also Flushing any pending writes)
