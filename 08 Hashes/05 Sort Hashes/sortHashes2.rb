afe5b3f860d102320fd9fe9622a647a8b8f6ec31
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-07 15:52:07 +0700

    Add files via upload

---
+++ 08 Hashes/05 Sort Hashes/sortHashes2.rb
@@ -0,0 +1,8 @@
+#! /usr/local/bin/ruby
+
+string = {7 => "bakwan", 5 => "75697485", 6 => "RISOLLES", 2 => "Rempeyek", 3 => "Bala-Bala", 4 => "83657185", 8 => "Kroket", 1 => "PERKEDEL"}
+
+p string
+
+print string.sort
+print "\n"
