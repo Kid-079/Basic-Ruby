6a9ac6a45227e64c2d24249eb553ab628e9b2d5a
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-07 15:51:49 +0700

    Add files via upload

---
+++ 08 Hashes/04 Merge Hashes/mergeHashes5.rb
@@ -0,0 +1,8 @@
+#! /usr/local/bin/ruby
+
+string1 = {1 => "bakwan", 2 => "75697485", 3 => "RISOLLES", 4 => "Rempeyek", 5 => "Bala-Bala", 6 => "83657185", 7 => "Kroket", 8 => "PERKEDEL"}
+string2 = {1 => "pastel", 2 => "7569846578", 3 => "Rengginang", 4 => "Emping", 5 => "AMPYANG", 6 => "7169848575", 7 => "79786869", 8 => "Melinjo"}
+
+string1.merge(string2)
+print string1
+print "\n"
