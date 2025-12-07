c2723715a58de90b24ecb68506685d5884fb0b6d
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2025-12-07 15:53:16 +0700

    Add files via upload

---
+++ 08 Hashes/07 Convert Hashes/convertHashes5.rb
@@ -0,0 +1,11 @@
+#! /usr/local/bin/ruby
+
+hashes = {1 => "pastel", 2 => "7569846578", 3 => "Rengginang", 4 => "Emping", 5 => "AMPYANG", 6 => "7169848575", 7 => "79786869", 8 => "Melinjo", 9 => "bakwan", 10 => "75697485", 11 => "RISOLLES", 12 => "Rempeyek", 13 => "Bala-Bala", 14 => "83657185", 15 => "Kroket", 16 => "PERKEDEL", 17 => "Seblak", 18 => "766977806982", 19 => "CIRENG", 20 => "mendoan"}
+
+data1 = hashes.to_s     # Convert to String 
+data2 = hashes.to_hash  # Convert to Hash 
+
+print data1
+print "\n"
+print data2
+print "\n"
---
+++ 08 Hashes/07 Convert Hashes/convertHashes6.rb
@@ -0,0 +1,9 @@
+#! /usr/local/bin/ruby
+
+array = [[1, "RENGGINANG"], [2, "Seblak"], [3, "7569846578"], [4, "Pastel"], [5, "Ampyang"],[6, "7169848575"], [7, "melinjo"], [8, "BAKWAN"], [9, "79786869"], [10, "risolles"],[11, "REMPEYEK"], [12, "BALA-BALA"], [13, "766977806982"], [14, "KROKET"], [15, "cireng"],[16, "Perkedel"], [17, "83657185"], [18, "mendoan"], [19, "Emping"], [20, "75697485"]]
+
+hashes = array.to_h  # Convert Array to Hashes
+
+print hashes
+print "\n"
+
