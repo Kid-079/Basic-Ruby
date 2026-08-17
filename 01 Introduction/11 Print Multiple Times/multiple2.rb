1d60343bd12631ac864aed8fcb773feb88a9320f
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 10:52:43 +0700

    Update multiple2.rb

--- 01 Introduction/11 Print Multiple Times/multiple2.rb
+++ 01 Introduction/11 Print Multiple Times/multiple2.rb
@@ -1,5 +1,5 @@
 #! /usr/local/bin/ruby
 
-5.times { print "Hello " }
+5.times { print "Enjoy Life And Do Whats Make You Happy " }
 # 5.times { print "Hello \n" }
 # 5.times { print "Always Fight For The Things You Love \n" }
