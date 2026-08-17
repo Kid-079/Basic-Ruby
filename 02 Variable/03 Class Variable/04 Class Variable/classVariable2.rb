70500e6165dd4e51f16255d5bf8ad662d217e594
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 15:12:43 +0700

    Update classVariable2.rb

--- 02 Variable/03 Class Variable/04 Class Variable/classVariable2.rb
+++ 02 Variable/03 Class Variable/04 Class Variable/classVariable2.rb
@@ -1,23 +1,23 @@
 #! /usr/local/bin/ruby
 
-class A
-	@@total_A = 0
+class Character
+	@@total_character = 0
 	def initialize(names,address)
-		@a_names = names
-		@a_address  = address
+		@character_names = names
+		@character_address  = address
 	end
 
 	def show_names
-		@a_names
+		@character_names
 	end
 
 	def show_address
-		@a_address
+		@character_address
 	end
 end
 
-person1 = A.new("A","123")
-person2 = A.new("B","456")
+person1 = Character.new("Write__Name_1","not_detected")
+person2 = Character.new("Write__Name_2","not_detected")
 
 puts person1.show_names
 puts person1.show_address
