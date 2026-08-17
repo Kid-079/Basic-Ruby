3e2d139cd1e247f26855ec9aae065991eadaa4ed
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 18:33:05 +0700

    Update ifelse4.rb

--- 04 Conditional Statement/02 ifElse/ifelse4.rb
+++ 04 Conditional Statement/02 ifElse/ifelse4.rb
@@ -1,11 +1,11 @@
 #! /usr/local/bin/ruby
 
-a = 4
-b = 4
+a = 5
+b = 5
 
-if a === 0 || b === 5
+if a === 0 || b === 7
 	puts "Moons"
-elsif a === 5 || b === 0
+elsif a === 7 || b === 0
 	puts "Suns"
 else
 	puts "Data Not Found"
