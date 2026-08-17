1890d21ddcf119e2198c83f9761683434b44c9b6
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 10:56:22 +0700

    Update multiple1.rb

--- 01 Introduction/11 Print Multiple Times/multiple1.rb
+++ 01 Introduction/11 Print Multiple Times/multiple1.rb
@@ -1,4 +1,4 @@
 #! /usr/local/bin/ruby
 
-puts "Every Strom Calms Down When You Stay True To Yourself " * 4 
+puts "Every Storm Calms Down When You Stay True To Yourself " * 4 
 #puts "Never Give Up On Your Dreams " * 5
