7f027c0df26430ad7f514ebe574b317b2a6278c4
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 18:18:23 +0700

    Update if2.rb

--- 04 Conditional Statement/01 if/if2.rb
+++ 04 Conditional Statement/01 if/if2.rb
@@ -1,8 +1,8 @@
 #! /usr/local/bin/ruby
 
-string1 = "A"
-string2 = "B"
+string1 = "Moons"
+string2 = "Suns"
 
-if string1 == "A" && string2 == "B"
-	puts "Hello"
+if string1 == "Moons" && string2 == "Suns"
+	puts "Moons And Suns Shining Bright"
 end
