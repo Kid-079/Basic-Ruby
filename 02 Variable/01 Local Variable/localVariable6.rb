a3781f7e6aa79e799b760d75b263855895d95fab
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 11:29:43 +0700

    Update localVariable6.rb

--- 02 Variable/01 Local Variable/localVariable6.rb
+++ 02 Variable/01 Local Variable/localVariable6.rb
@@ -3,7 +3,7 @@
 STRING_1 = 'EVERY STORM CALMS DOWN, WHEN YOU STAY TRUE TO YOURSELF!'
 
 class Beauty
-	STRING_2 = proc {'EFGH'}
+	STRING_2 = proc {'ENJOY LIFE AND DO WHATS MAKE YOU HAPPY'}
 	def data
 		::STRING_1   # Variable Global  
 	end
