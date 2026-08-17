1612606bb0b654a2e028259a5a01504b35eae7eb
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 09:27:49 +0700

    Update hello1.rb

--- 01 Introduction/01 Basic Syntax/hello1.rb
+++ 01 Introduction/01 Basic Syntax/hello1.rb
@@ -1,4 +1,4 @@
-puts "Hello Hello"    # -> with newline
+puts "Be Proud Of How Hard You Are Trying"    # -> with newline
 #puts "Sometimes, Some Of Your Greatest Pain Become Your Greatest Strength"    # -> with newline
-print "Hello Hello"   # -> without newline
+print "Focus On Improving Yourself, Not Proving Yourself"   # -> without newline
 #print "Get Up, Stand Up, Dont Give Up"   # -> without newline
