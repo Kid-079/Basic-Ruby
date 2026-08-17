7d5ecd00ed9c600b945a1ff2c0edba5715f6fced
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 19:24:08 +0700

    Update unless2.rb

--- 04 Conditional Statement/03 unless/unless2.rb
+++ 04 Conditional Statement/03 unless/unless2.rb
@@ -1,11 +1,11 @@
 #! /usr/local/bin/ruby
 
-char = "A"
+char = "Moons"
 
-if char == "A"
-	puts "Char A"
-elsif char == "B"
-	puts "Char B"
+if char == "Moons"
+	puts "The Moon Is Always Full, Even When We Cant See It"
+elsif char == "Suns"
+	puts "When We See The Shadow, We See The Sunrise. But When We Cannot See The Shadow, We See The Sunset"
 else
-	puts "Not A or B"
+	puts "Data Not Found"
 end
