3c287628c2a520791452a975b238140509cf0387
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 16:41:52 +0700

    Update accessorMethod2.rb

--- 02 Variable/05 Accessor Method/accessorMethod2.rb
+++ 02 Variable/05 Accessor Method/accessorMethod2.rb
@@ -1,19 +1,19 @@
 #! /usr/local/bin/ruby
 
-class A
-	attr_reader :hello #getter
-	attr_writer :hello #setter
+class Journey
+	attr_reader :stronger #getter
+	attr_writer :stronger #setter
 end
 
-expression = A.new
+expression = Journey.new
 
-expression.hello = "Hello, Hello"
-puts expression.hello
+expression.stronger = "Everyday Is A Gift, Thats Why We Call It The Present"
+puts expression.stronger
 
 print expression.instance_variables.sort
 print "\n"
-print A.instance_methods.sort - Object.instance_methods
+print Journey.instance_methods.sort - Object.instance_methods
 print "\n"
 
 # puts expression.instance_variables.sort
-# puts A.instance_methods.sort - Object.instance_methods
+# puts Journey.instance_methods.sort - Object.instance_methods
