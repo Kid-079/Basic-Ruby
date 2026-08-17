2f4702a3c2437e2a41d13f82ff002f4605099cd9
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 20:14:51 +0700

    Add files via upload

---
+++ 04 Conditional Statement/05 Loops/04 Break Statement/break5.rb
@@ -0,0 +1,8 @@
+#! /usr/local/bin/ruby
+
+for a in 0...20
+	if a >= 10 then 
+		break
+	end
+	puts "Value a : #{a}"
+end
---
+++ 04 Conditional Statement/05 Loops/04 Break Statement/break6.rb
@@ -0,0 +1,8 @@
+#! /usr/local/bin/ruby
+
+for a in 0..20
+	if a > 10 then 
+		puts "Value a : #{a}"
+		break
+	end
+end
