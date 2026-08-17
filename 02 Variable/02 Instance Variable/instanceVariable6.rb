ef8813f0e5f5826ae0bedb1353493be61430ca7c
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-17 12:48:46 +0700

    Update instanceVariable6.rb

--- 02 Variable/02 Instance Variable/instanceVariable6.rb
+++ 02 Variable/02 Instance Variable/instanceVariable6.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-class A
+class Dreams
 	def initialize(names)
 		@a_names = names
 	end
@@ -10,11 +10,11 @@ class A
 	end
 
 	def text
-		"#{@a_names} Hello, Hello"
+		"#{@a_names} Dont Quit Your Day Dream"
 	end
 end
 
-person1 = A.new("name_A")
+person1 = Dreams.new("Write_Some_Name_1")
 
 puts person1.show_name
 puts person1.text
