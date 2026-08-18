38c43b4596f16a4d3de26e2b63942dc0b8d8c68c
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 23:01:33 +0700

    Update reverseString3.rb

--- 06 Strings/04 Manipulate String/05 Reverse String/reverseString3.rb
+++ 06 Strings/04 Manipulate String/05 Reverse String/reverseString3.rb
@@ -1,6 +1,18 @@
 #! /usr/local/bin/ruby
 
-string = "TUPURES"
+string1 = "ELGGURTS"
+string2 = "YENRUOJ"
+string3 = "SMAERD"
+string4 = "TIRIPS"
+string5 = "REGNORTS"
 
-print string.reverse
+print string1.reverse
+print "\n"
+print string2.reverse
+print "\n"
+print string3.reverse
+print "\n"
+print string4.reverse
+print "\n"
+print string5.reverse
 print "\n"
