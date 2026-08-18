f6f239c4f4bfe977cf7a72e84ec5b777a5b30e65
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 16:32:47 +0700

    Update concatenate2.rb

--- 06 Strings/02 Concatenate String/concatenate2.rb
+++ 06 Strings/02 Concatenate String/concatenate2.rb
@@ -1,7 +1,7 @@
 #! /usr/local/bin/ruby
 
-string1 = "Journey"
-string2 = "DREAMS"
+string1 = "Let Our Dreams, Be Our Wings"
+string2 = "LIFE IS A JOURNEY, ENJOY THE RIDE"
 
 print string1.concat(string2)
 print "\n"
