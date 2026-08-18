adea91e06968a8224274770fdc2e1b3db582c548
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 10:49:11 +0700

    Update yieldStatement4.rb

--- 05 Methods/04 Yield Statement/yieldStatement4.rb
+++ 05 Methods/04 Yield Statement/yieldStatement4.rb
@@ -5,7 +5,7 @@ def string
 	yield
 end
 
-string = "Hello"     # --> Block Given
+string = "Trust The Process, Enjoy The Journey"     # --> Block Given
 #string = "If I Cannot Do Great Things, I Can Do Small Things In A Great Way"     # --> Block Given
 
 puts string          # --> No Eror : Program Success
