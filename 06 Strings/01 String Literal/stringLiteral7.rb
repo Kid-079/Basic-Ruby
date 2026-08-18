27f8f40eab2b9637719d4c9b4e4f4972ecec3beb
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 15:39:36 +0700

    Update stringLiteral7.rb

--- 06 Strings/01 String Literal/stringLiteral7.rb
+++ 06 Strings/01 String Literal/stringLiteral7.rb
@@ -1,7 +1,7 @@
 #! /usr/local/bin/ruby
 
-string1 = String.new("ABCDE")
-string2 = String.new("abcde")
+string1 = String.new("GRATITUDE SWEETENS EVEN THE SMALLEST MOMENTS")
+string2 = String.new("gratitude sweetens even the smallest moments")
 
 data1 = string1.downcase
 data2 = string2.upcase
