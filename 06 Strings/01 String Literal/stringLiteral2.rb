874709767520d02aceedd01d61edb9bc2a372f74
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 15:03:55 +0700

    Update stringLiteral2.rb

--- 06 Strings/01 String Literal/stringLiteral2.rb
+++ 06 Strings/01 String Literal/stringLiteral2.rb
@@ -1,11 +1,11 @@
 #! /usr/local/bin/ruby
 
-a = "Hello, %s"
+passion = "Trust The Process, %s"
 
-puts a % "A"
-puts a % "B"
-puts a % "C"
+puts passion % "Enjoy The Journey"
+puts passion % "Enjoy The Moment"
+puts passion % "Enjoy Every Step"
 
 
 # % -> Method from the String Class that format a string
-#      like using sprintf("Hello, %s", "Hello!")
+#      like using sprintf("Hello, %s", "Trust The Process Enjoy The Journey!")
