f011f8208b380db01b231ebd8e671dae3ed4ce0c
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 16:42:29 +0700

    Update stringCompare1.rb

--- 06 Strings/03 String Compare/stringCompare1.rb
+++ 06 Strings/03 String Compare/stringCompare1.rb
@@ -1,7 +1,7 @@
 #! /usr/local/bin/ruby
 
-string1 = "This is a String"
-string2 = "This is a string"
+string1 = "Let Our Dreams, Be Our Wings"
+string2 = "Life Is A Journey, Enjoy The Ride"
 
 print string1 == string2
 print "\n"
