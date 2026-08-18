68b8b881505799f62bcc54f78a3a4dc406bdeefa
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 11:11:00 +0700

    Update procedures1.rb

--- 05 Methods/07 Proc Statement/procedures1.rb
+++ 05 Methods/07 Proc Statement/procedures1.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-proc = lambda { |name| puts "Hello, " + name}
+proc = lambda { |name| puts "A Good Heart, Keeps You Beautiful, " + name}
 
-proc.call "A"
-proc.call "B"
+proc.call "Write__Name_1"
+proc.call "Write__Name_2"
