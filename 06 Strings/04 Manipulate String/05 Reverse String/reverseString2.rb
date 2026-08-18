d6752e342bbecac24b96cb1b551c249e518ca105
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 22:15:58 +0700

    Update reverseString2.rb

--- 06 Strings/04 Manipulate String/05 Reverse String/reverseString2.rb
+++ 06 Strings/04 Manipulate String/05 Reverse String/reverseString2.rb
@@ -1,7 +1,13 @@
 #! /usr/local/bin/ruby
 
-font = "abcde"
+font = "abcdefghijklmnopqrstuvwxyz"
+string1 = "Stay Patient And Trust Our Journey"
+string2 = "Life Is A Journey Of Small Steps That Lead Us Toward Brighter Future"
 
 font.reverse!
+string1.reverse!
+string2.reverse!
 
 p font
+p string1
+p string2
