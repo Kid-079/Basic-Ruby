c79adee2a03fe1f6c1afcb5ded98740e24d3d17f
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 10:48:33 +0700

    Update yieldStatement2.rb

--- 05 Methods/04 Yield Statement/yieldStatement2.rb
+++ 05 Methods/04 Yield Statement/yieldStatement2.rb
@@ -4,5 +4,5 @@ def data
 	yield
 end
 
-data {puts "Hello Hello"}
+data {puts "Trust The Process, Enjoy The Journey"}
 #data {puts "If I Cannot Do Great Things, I Can Do Small Things In A Great Way"}
