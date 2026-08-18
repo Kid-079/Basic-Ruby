3097121cbccef695ff6018e84d70d97ffda83f3d
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-18 20:31:25 +0700

    Update incrementString2.rb

--- 06 Strings/04 Manipulate String/04 Increment String/incrementString2.rb
+++ 06 Strings/04 Manipulate String/04 Increment String/incrementString2.rb
@@ -1,17 +1,20 @@
 #! /usr/local/bin/ruby
 
-
 string1 = "z"
-string2 = "zz"
+string2 = "zzz"
+string3 = "zy"
+string4 = "zxy"
 
 # Character Set use ASCII
-string3 = "?"
+string5 = "!!"
 
-string4 = "000.0"
-string5 = "000"
+string6 = "0000.00"
+string7 = "0000"
 
 puts string1.next
 puts string2.next
 puts string3.next
 puts string4.next
 puts string5.next
+puts string6.next
+puts string7.next
