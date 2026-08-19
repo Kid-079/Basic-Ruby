92cbe940fe80233a267676ac0c98615e9de4c418
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 20:43:38 +0700

    Update matchString1.rb

--- 06 Strings/08 Regular Expression/04 Match Strings/matchString1.rb
+++ 06 Strings/08 Regular Expression/04 Match Strings/matchString1.rb
@@ -2,6 +2,9 @@
 
 string = "Do Your Best And Forget The Rest"
 
+puts string.match /\w{2}/     # Match String
 puts string.match /\w{6}/     # Match String
-puts string.match /\w{15}/    # Not Match
-puts string.match /^\w{5}$/   # Not Match
+puts string.match /\w{8}/     # Not Match
+puts string.match /\w{32}/    # Not Match
+puts string.match /^\w{4}$/   # Not Match
+puts string.match /Best/   
