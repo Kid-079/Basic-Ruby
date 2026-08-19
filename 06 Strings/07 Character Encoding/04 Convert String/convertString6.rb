137f7b0a1de96a532441e685a92180eba7bc5061
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 17:20:28 +0700

    Update convertString6.rb

--- 06 Strings/07 Character Encoding/04 Convert String/convertString6.rb
+++ 06 Strings/07 Character Encoding/04 Convert String/convertString6.rb
@@ -1,14 +1,14 @@
 #! /usr/local/bin/ruby
 
-data = "ABCDE"
+data = "KETOPRAK KEJU"
 
 puts data.to_sym          # :ABCDE
 puts data.intern          # :ABCDE
 puts data.to_s            # "ABCDE" 
-puts "ABC".to_sym         # :ABCDE
-puts :ABC.to_s            # "ABCDE"
-puts :ABC.id2name         # "ABCDE"
-puts data == :ABCDE.to_s
+puts "KETOPRAK".to_sym         # :ABCDE
+puts :KETOPRAK.to_s            # "ABCDE"
+puts :KETOPRAK.id2name         # "ABCDE"
+puts data == :KETOPRAK_KEJU.to_s
 
 
 hashes = {:lang1 => English}
