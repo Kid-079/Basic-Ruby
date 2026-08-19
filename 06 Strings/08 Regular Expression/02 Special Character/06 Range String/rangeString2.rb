83b90a568e975a20ef03f95b0ee1cb5c6d9b8d60
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 20:00:45 +0700

    Update rangeString2.rb

--- 06 Strings/08 Regular Expression/02 Special Character/06 Range String/rangeString2.rb
+++ 06 Strings/08 Regular Expression/02 Special Character/06 Range String/rangeString2.rb
@@ -5,6 +5,6 @@ def names(string)
 end
 
 # Return Indeks
-puts names("crunch")         # string found, i => indeks 1
+puts names("crunch")         # string found, u => indeks 2
 puts names("zdfskfwxe")      # string found, e => indeks 3
 puts names("hhhhhhhh")       # string not found
