31a5260fb6d617bd22daa19f155ea3b1831fcc50
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 18:50:22 +0700

    Update escapeCharacter2.rb

--- 06 Strings/08 Regular Expression/02 Special Character/01 Escape Character/escapeCharacter2.rb
+++ 06 Strings/08 Regular Expression/02 Special Character/01 Escape Character/escapeCharacter2.rb
@@ -2,11 +2,14 @@
 
 string1 = "7l6e8e8z6z8a7a6t7t8"
 string2 = "8-5-3-4-5-2-5-9-4-5"
+string3 = "3.2.6.9.7.7.0.8.8.3"
 
 # NOT USE ESCAPE CHARACTER
 puts string1.match(/\d.\d.\d.\d.\d.\d.\d.\d.\d.\d/) 
-puts string2.match(/\d.\d.\d.\d.\d.\d.\d.\d.\d.\d/) 
+puts string2.match(/\d.\d.\d.\d.\d.\d.\d.\d.\d.\d/)
+puts string3.match(/\d.\d.\d.\d.\d.\d.\d.\d.\d.\d/) 
 
 # USE ESCAPE CHARACTER
 puts string1.match(/\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d/)  # Letter Not Match
-puts string2.match(/\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d/)  # Symbol Matches
+puts string2.match(/\d\-\d\-\d\-\d\-\d\-\d\-\d\-\d\-\d\-\d/)  # Symbol Matches
+puts string3.match(/\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d/)  # Symbol Matches
