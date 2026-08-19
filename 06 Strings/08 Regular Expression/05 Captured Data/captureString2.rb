adb0a45a6122988c28e35ee40a74054426797b10
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 22:38:22 +0700

    Update captureString2.rb

--- 06 Strings/08 Regular Expression/05 Captured Data/captureString2.rb
+++ 06 Strings/08 Regular Expression/05 Captured Data/captureString2.rb
@@ -1,6 +1,8 @@
 #! /usr/local/bin/ruby
 
-string = "ABC 123".match /(\w+) (\d+)/
+string = "JOURNEY DREAMS STRUGGLE 7610570101".match /(\w+) (\d+)/
+#string = "JOURNEY DREAMS STRUGGLE 7610570101".match /(\w+) (\w+) (\d+)/
+#string = "JOURNEY DREAMS STRUGGLE 7610570101".match /(\w+) (\w+) (\w+) (\d+)/
 
 puts string[0]
 puts string[1]
