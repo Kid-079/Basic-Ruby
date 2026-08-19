dadc2c5d0ee09946ef84d9fbad3d5f531c664ed6
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 17:54:57 +0700

    Update regex2.rb

--- 06 Strings/08 Regular Expression/01 Defined Regex/regex2.rb
+++ 06 Strings/08 Regular Expression/01 Defined Regex/regex2.rb
@@ -2,7 +2,7 @@
 
 string = "Life Is A Journey, Enjoy The Ride"
 # string = "Every Chapter Of Life Has Its Own Beauty"
-# string = "Be Patient, Some Wounds Take Time To Heal!"
+# string = "Be Patient, Some Wounds Take Time To Heal!!!!!"
 
 if (string =~ /Journey/)
 	puts "Enjoy The Ride"
