a06dcc4c039a4cfe3d05b6db6f3116045312e990
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 15:59:02 +0700

    Update stringArray1.rb

--- 06 Strings/06 Delimited Input/stringArray1.rb
+++ 06 Strings/06 Delimited Input/stringArray1.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-print %w(Value #{1 + 1})     # no interpolation
+print %w(Value #{5 + 5})     # no interpolation
 print "\n"
-print %W(Value = #{1 + 1})   # with interpolation
+print %W(Value = #{5 + 5})   # with interpolation
 print "\n"
