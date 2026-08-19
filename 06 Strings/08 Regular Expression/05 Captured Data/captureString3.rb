d2cc0cfd55664116d718165a7c5fb88d4cfef807
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 22:49:06 +0700

    Update captureString3.rb

--- 06 Strings/08 Regular Expression/05 Captured Data/captureString3.rb
+++ 06 Strings/08 Regular Expression/05 Captured Data/captureString3.rb
@@ -1,7 +1,12 @@
 #! /usr/local/bin/ruby
 
-string1 = "ABC 123".match /((\w+) (\d+))/
-string2 = "ABC 123".match /(((\w+) (\d+)))/
+# string1 = "JOURNEY DREAMS STRUGGLE 7610570101".match /((\w+) (\d+))/
+# string1 = "JOURNEY DREAMS STRUGGLE 7610570101".match /((\w+) (\w+) (\d+))/
+string1 = "JOURNEY DREAMS STRUGGLE 7610570101".match /((\w+) (\w+) (\w+) (\d+))/
+
+string2 = "Journey Dreams Struggle 7610570101".match /(((\w+) (\d+)))/
+# string2 = "Journey Dreams Struggle 7610570101".match /(((\w+) (\w+) (\d+)))/
+# string2 = "Journey Dreams Struggle 7610570101".match /(((\w+) (\w+) (\w+) (\d+)))/
 
 puts string1[0]
 puts string1[1]
