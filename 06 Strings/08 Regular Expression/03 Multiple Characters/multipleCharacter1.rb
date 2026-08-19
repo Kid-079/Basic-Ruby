0ef77a5989c0b666da7372c2e747340f23f14655
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 20:27:42 +0700

    Update multipleCharacter1.rb

--- 06 Strings/08 Regular Expression/03 Multiple Characters/multipleCharacter1.rb
+++ 06 Strings/08 Regular Expression/03 Multiple Characters/multipleCharacter1.rb
@@ -4,8 +4,8 @@ def number?(string)
 	!!(string =~ /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}\.\d{1,3}$/)
 end
 
-puts number?("66.97.76.65.68.79")  # TRUE
-puts number?("115.69.109.85.114")    # FALSE
+puts number?("66.97.76.65.68.79")          # TRUE
+puts number?("115.69.109.85.114")          # FALSE
 puts number?("98.65.116.97.103.111.82")    # FALSE
 
 =begin
