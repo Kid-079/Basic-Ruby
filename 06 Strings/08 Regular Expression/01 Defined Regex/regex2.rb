54021c50fd3b5e6cda99a116dd70528a9047f1bb
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 17:51:46 +0700

    Update regex2.rb

--- 06 Strings/08 Regular Expression/01 Defined Regex/regex2.rb
+++ 06 Strings/08 Regular Expression/01 Defined Regex/regex2.rb
@@ -4,10 +4,10 @@ string = "Be Patient, Some Wounds Take Time To Heal!"
 # string = "Every Chapter Of Life Has Its Own Beauty"
 # string = "Life Is A Journey, Enjoy The Ride"
 
-if (string =~ /String/)
-	puts "STRING 1"
-elsif (string =~ /Character/)
-	puts "STRING 2"
+if (string =~ /Journey/)
+	puts "Enjoy The Ride"
+elsif (string =~ /Beauty/)
+	puts "Every Chapter, Has Its Own Beauty"
 else 
 	puts "UNDEFINED"
 end
