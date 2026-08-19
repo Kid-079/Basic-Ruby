bb63d0f3b68f072fe20ea47338274cdf684b06f6
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 19:21:44 +0700

    Update escapeCharacter1.rb

--- 06 Strings/08 Regular Expression/02 Special Character/01 Escape Character/escapeCharacter1.rb
+++ 06 Strings/08 Regular Expression/02 Special Character/01 Escape Character/escapeCharacter1.rb
@@ -1,7 +1,8 @@
 #! /usr/local/bin/ruby
 
-string1 = "1a2b3"
-string2 = "1-2-3"
+string1 = "j3j5o6u3r4r2n7e8e5y9"
+string2 = "8.1.5.1.3.7.0.5.5.0"
+string3 = "7-4-7-6-5-6-8-9-5-5"
 
 # NOT USE ESCAPE CHARACTER
 puts string1.match(/\d.\d.\d/) 
