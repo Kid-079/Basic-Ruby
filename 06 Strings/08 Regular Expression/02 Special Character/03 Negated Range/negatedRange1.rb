ca56c921101f6b7ed2572e475bbecb8b15810d71
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 19:46:33 +0700

    Update negatedRange1.rb

--- 06 Strings/08 Regular Expression/02 Special Character/03 Negated Range/negatedRange1.rb
+++ 06 Strings/08 Regular Expression/02 Special Character/03 Negated Range/negatedRange1.rb
@@ -4,8 +4,9 @@ def letter(string)
 	string =~ /[^A-Z]/
 end
 
-puts letter("123H")
-puts letter("M123")
-puts letter("123")
-puts letter("m123")
-puts letter("123h")
+puts letter("832027O")
+puts letter("N219675")
+puts letter("4735c")
+puts letter("o8481")
+puts letter("26275")
+puts letter("49095m")
