7aa2c6b9207037062c2d64f329ef0d07d62c376e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 19:55:47 +0700

    Update rangeNumber2.rb

--- 06 Strings/08 Regular Expression/02 Special Character/05 Range Number/rangeNumber2.rb
+++ 06 Strings/08 Regular Expression/02 Special Character/05 Range Number/rangeNumber2.rb
@@ -4,9 +4,9 @@ def number(string)
 	string =~ /[0-9]/
 end
 
-puts number("age is twenty 5")      # String Found, Return total indeks
-#puts number("age is fifty 5")       # String Found, Return total indeks  
-#puts number("age is fourty 5")       # String Found, Return total indeks  
-puts number("age is twenty")        # String Not Found
+puts number("number is twenty 5")      # String Found, Return total indeks
+#puts number("number is fifty 5")       # String Found, Return total indeks  
+#puts number("number is fourty 5")       # String Found, Return total indeks  
+puts number("number is twenty")        # String Not Found
 
 
