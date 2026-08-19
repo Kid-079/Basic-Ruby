80a4b7983c47bfbccc706341f09e3cccf311b551
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 19:23:52 +0700

    Update escapeCharacter1.rb

--- 06 Strings/08 Regular Expression/02 Special Character/01 Escape Character/escapeCharacter1.rb
+++ 06 Strings/08 Regular Expression/02 Special Character/01 Escape Character/escapeCharacter1.rb
@@ -5,9 +5,11 @@ string2 = "8.1.5.1.3.7.0.5.5.0"
 string3 = "7-4-7-6-5-6-8-9-5-5"
 
 # NOT USE ESCAPE CHARACTER
-puts string1.match(/\d.\d.\d/) 
-puts string2.match(/\d.\d.\d/) 
+puts string1.match(/\d.\d.\d.\d.\d.\d.\d.\d.\d.\d/) 
+puts string2.match(/\d.\d.\d.\d.\d.\d.\d.\d.\d.\d/)
+puts string3.match(/\d.\d.\d.\d.\d.\d.\d.\d.\d.\d/)
 
 # USE ESCAPE CHARACTER
-puts string1.match(/\d\.\d\.\d/)  # Letter Not Match
-puts string2.match(/\d\.\d\.\d/)  # Symbol Matches
+puts string1.match(/\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d/)  # Letter Not Match
+puts string2.match(/\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d/)  # Symbol Matches
+puts string3.match(/\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d\.\d/)  # Symbol Matches
