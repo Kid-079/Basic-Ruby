a1cb1feb0a74a0761ed613be5699cd89fbdbebd7
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 16:14:43 +0700

    Update symbolsArray1.rb

--- 06 Strings/06 Delimited Input/symbolsArray1.rb
+++ 06 Strings/06 Delimited Input/symbolsArray1.rb
@@ -3,14 +3,14 @@
 # SYMBOLS
 print "SYMBOL"
 print "\n"
-print %s(value #{1+1}) # no interpolation
+print %s(value #{5+5}) # no interpolation
 print "\n"
 print "\n"
 
 # ARRAY
 print "ARRAY OF SYMBOL"
 print "\n"
-print %i(value #{1 + 1})  # no interpolation
+print %i(value #{5 + 5})  # no interpolation
 print "\n"
-print %I(value #{1 + 1})  # with interpolation
+print %I(value #{5 + 5})  # with interpolation
 print "\n"
