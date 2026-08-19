28418459158c06f7cd7e6c0b8964e0ddcf8f003c
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 16:15:08 +0700

    Update symbolsArray2.rb

--- 06 Strings/06 Delimited Input/symbolsArray2.rb
+++ 06 Strings/06 Delimited Input/symbolsArray2.rb
@@ -2,9 +2,9 @@
 
 # SYMBOLS
 puts "SYMBOL"
-puts %s(value #{1+1}) # no interpolation
+puts %s(value #{5+5}) # no interpolation
 puts
 # ARRAY
 puts "ARRAY OF SYMBOL"
-puts %i(value#{1+1})  # no interpolation
-puts %I(value=#{1 + 1})  # with interpolation
+puts %i(value #{5+5})  # no interpolation
+puts %I(value=#{5 + 5})  # with interpolation
