499563fcdb9341f125a1f349f5a896a94d2eb8d3
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-19 23:12:26 +0700

    Update accessElement5.rb

--- 07 Arrays/02 Access Elements/accessElement5.rb
+++ 07 Arrays/02 Access Elements/accessElement5.rb
@@ -1,6 +1,10 @@
 #! /usr/local/bin/ruby
 
 number = %w[K E R U P U K]
+# number = %w[L U M P I A]
+# number = %w[E M P I N G]
+# number = %w[S E R A B I]
+# number = %w[P E C E L]
 
 # ACCESS SPECIFIC ELEMENT
 print number[0]
