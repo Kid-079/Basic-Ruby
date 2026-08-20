5a0413e8dbbda4bcbf3f8a053343c1101d1897b1
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-20 11:31:40 +0700

    Update arrayOperation3.rb

--- 07 Arrays/04 Set Operation/arrayOperation3.rb
+++ 07 Arrays/04 Set Operation/arrayOperation3.rb
@@ -1,13 +1,17 @@
 #! /usr/local/bin/ruby
 
-array = [2, 4, 7, 1, 9, 0]
+array1 = [2, 4, 7, 1, 9, 0]
+# array2 = [4, 7, 6, 0, 5, 8]
+# array3 = [3, 7, 2, 4, 5, 9]
+# array4 = [9, 6, 4, 5, 4, 8]
+# array5 = [8, 7, 2, 2, 3, 5]
 
 # SORT ELEMENT
-array.sort!
-print array
+array1.sort!
+print array1
 print "\n"
 
 # REVERSE ELEMENT
-array.reverse!
-print array
+array1.reverse!
+print array1
 print "\n"
