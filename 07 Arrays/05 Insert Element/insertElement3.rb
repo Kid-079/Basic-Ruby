cea5c83238dab2aa1e4b46e6ad66a1290421922d
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-20 12:16:38 +0700

    Update insertElement3.rb

--- 07 Arrays/05 Insert Element/insertElement3.rb
+++ 07 Arrays/05 Insert Element/insertElement3.rb
@@ -2,9 +2,9 @@
 
 array = %w[E M P I N G]
 
-array.insert(5, 1, 2, 3)
-array.insert(8, 4, 7, 1)
-array.insert(7, 1, 3, 8)
+array.insert(5, 2, 1, 3)
+# array.insert(8, 4, 7, 1)
+# array.insert(7, 1, 3, 8)
 
 print array
 print "\n"
