7543ab2379f13323be697010b266d41a34df953e
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-20 11:11:59 +0700

    Update arrayOperation2.rb

--- 07 Arrays/04 Set Operation/arrayOperation2.rb
+++ 07 Arrays/04 Set Operation/arrayOperation2.rb
@@ -1,14 +1,14 @@
 #! /usr/local/bin/ruby
 
-a = ["A","B","C"]
-b = ["A"]
+a = ["B","A","T","A","G","O","R"]
+b = ["T","E","M","P","E"]
 
 
 # Compare Array
 print a == b
 print "\n"
 
-b.push "B", "C"
+b.push "G", "E", "J", "R", "O", "T"
 print a == b
 print "\n"
 
