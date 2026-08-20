1022f9799f7f649799e2caf4c5bb66939e05a8ca
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-20 23:17:51 +0700

    Update createHashes1.rb

--- 08 Hashes/01 Create Hashes/createHashes1.rb
+++ 08 Hashes/01 Create Hashes/createHashes1.rb
@@ -1,12 +1,19 @@
 #! /usr/local/bin/ruby
 
-string = Hash.new("abc")
+string = Hash.new("semur jengkol")
 
 puts "#{string[0]}"
 puts "#{string[2]}"
+puts "#{string[0][6]}"
+puts "#{string[1][4]}"
+puts "#{string[1][4][9]}"
 puts
 puts string.empty?
 puts string[1].empty?
 puts
 puts string.length
 puts string.size
+puts
+puts string[1].length
+puts string[0].size
+
