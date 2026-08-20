958d5905cc6dc72564e6437475752448009dbe52
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-20 11:48:30 +0700

    Update insertElement2.rb

--- 07 Arrays/05 Insert Element/insertElement2.rb
+++ 07 Arrays/05 Insert Element/insertElement2.rb
@@ -1,13 +1,17 @@
 #! /usr/local/bin/ruby
 
-array = %w[A B C]
+array = %w[L U M P I A]
 
 array.pop 2     # LIFO
 print array
 print array
 print "\n"
 
-array.push "D"
+array.push "E" "M" "P" "I" "N" "G"
+print array
+print "\n"
+
+array.push "P", "E", "D", "A", "S"
 print array
 print "\n"
 
