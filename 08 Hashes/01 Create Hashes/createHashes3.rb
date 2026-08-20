24073e24e3beb0f6eaa467d3a58c71ca60e1f3ed
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 00:34:21 +0700

    Update createHashes3.rb

--- 08 Hashes/01 Create Hashes/createHashes3.rb
+++ 08 Hashes/01 Create Hashes/createHashes3.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-string = Hash["a" => [30, "batagor"], "b" => [1, 2, 3, 4, 5], "c" => ["Lumpia", 55, "EMPING"]]
+string = Hash["a" => [30, "batagor"], "b" => [5,3,8,5,1,1,2,8,7,4,8,1,1,4,4,3,6,9,7,6], "c" => ["Lumpia", 55, "EMPING"]]
 
 print string
 print "\n"
