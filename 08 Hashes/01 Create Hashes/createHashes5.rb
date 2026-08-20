7090f85e8094bd055437580fad72f224f65e0687
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 01:32:31 +0700

    Update createHashes5.rb

--- 08 Hashes/01 Create Hashes/createHashes5.rb
+++ 08 Hashes/01 Create Hashes/createHashes5.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-string1 = Hash[:name, "A", :type, "info", :status, "user-bujang"]
+string1 = Hash[:name, "BUJANG", :type, "info", :status, "user-bujang"]
 string2 = Hash[1 => "ABC", 2 => "abc", 3 => "Hello"]
 
 print string1
