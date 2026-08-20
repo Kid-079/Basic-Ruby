0990b5e190d87f76cbc547852999ad59520870b3
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-20 23:46:27 +0700

    Update createHashes2.rb

--- 08 Hashes/01 Create Hashes/createHashes2.rb
+++ 08 Hashes/01 Create Hashes/createHashes2.rb
@@ -1,15 +1,15 @@
 #! /usr/local/bin/ruby
 
-string = Hash["a" => 50, "b" => 55, "c" => 60, "d" => 65, "e" => 70, "f" => 75]
+string = Hash["a" => 50, "b" => 55, "c" => 60, "d" => 65, "e" => 70, "f" => 75, "g" => 80, "h" => 85, "i" => 90, "j" => 95]
 
 # SHOW HASHES
 print string
 print "\n"
 
 # ACCESS ELEMENT
-print "#{string['a']}"
+print "#{string['j']}"
 print "\n"
-print "#{string['b']}"
+print "#{string['f']}"
 print "\n"
 print string.empty?
 print "\n"
