9bec93e8fa7ec8d3d56a7f34e770e8cbe4961d74
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 19:16:53 +0700

    Update replaceHashes3.rb

--- 08 Hashes/03 Insert and Replace/replaceHashes3.rb
+++ 08 Hashes/03 Insert and Replace/replaceHashes3.rb
@@ -1,7 +1,6 @@
 #! /usr/local/bin/ruby
 
-hashes = Hash[1 => "ABC", 2 => "abc", 3 => "Hello", 4 => "abcde"]
-
+hashes = Hash[1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "917350697854", 5 => "76487884794357", 6 => "TERANG BULAN", 7 => "715184725260", 8 => "Ongol-Ongol", 9 => "URAP", 10 => "566575803352, 11 => "Dadar Kriwil", 12 => "79926869", 13 => "Emping", 14 => "244877568214", 15 => "Lumpia"]
 new_hashes = {"ABC" => 10}
 hashes.replace(new_hashes)
 
