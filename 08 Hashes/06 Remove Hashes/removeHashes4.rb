4fa230821fe4051584f0cd83de5f453e502e219b
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 20:06:27 +0700

    Update removeHashes4.rb

--- 08 Hashes/06 Remove Hashes/removeHashes4.rb
+++ 08 Hashes/06 Remove Hashes/removeHashes4.rb
@@ -1,8 +1,8 @@
 #! /usr/local/bin/ruby
 
-string = {1 => "123", 2 => "abc", 3 => "Abcde", 4 => "12345"}
+string = {1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "917350697854", 5 => "76487884794357", 6 => "TERANG BULAN", 7 => "715184725260", 8 => "Ongol-Ongol", 9 => "URAP", 10 => "566575803352"}
 
-string.delete_if {|key, value| value == "abc"}
+string.delete_if {|key, value| value == "batagor"}
 
 print string
 print "\n"
