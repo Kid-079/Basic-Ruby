7889d94b7d41f7a00afa9b8ca3053d6ffc3777e9
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 17:55:51 +0700

    Update accessHashes3.rb

--- 08 Hashes/02 Access Hashes/accessHashes3.rb
+++ 08 Hashes/02 Access Hashes/accessHashes3.rb
@@ -1,5 +1,5 @@
 #! /usr/local/bin/ruby
 
-string = Hash[1 => "ABC", 2 => "abc", 3 => "Hello", 4 => "abcde"]
+string = Hash[1 => "Dadar Kriwil", 2 => "Kerak Telor", 3 => "PASTEL", 4 => "Emping", 5 => "batagor", 6 => "TERANG BULAN", 7 => "Lumpia", 8 => "Ongol-Ongol", 9 => "lapis legit", 10 => "URAP" ]
 
 string.each {|key,value| puts "#{key} - #{value}"}
