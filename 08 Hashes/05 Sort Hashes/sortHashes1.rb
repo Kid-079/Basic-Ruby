241db4b2d8006132b73b98445a8a4395b0da4b01
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 19:59:54 +0700

    Update sortHashes1.rb

--- 08 Hashes/05 Sort Hashes/sortHashes1.rb
+++ 08 Hashes/05 Sort Hashes/sortHashes1.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-string = {3 => "ABC", 4 => "abc", 1 => "Hello", 2 => "abcde"}
+string = {5 => "76487884794357", 9 => "Ongol-Ongol", 8 => "batagor", 10 => "917350697854", 1 => "URAP", 3 => "5785685171", 7 => "Kerak Telor", 4 => "566575803352", 2 => "TERANG BULAN", 6 => "715184725260"}
 
 p string
 
