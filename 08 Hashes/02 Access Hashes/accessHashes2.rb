914f36315367eb82b1b9db23bf69510216691071
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 17:55:30 +0700

    Update accessHashes2.rb

--- 08 Hashes/02 Access Hashes/accessHashes2.rb
+++ 08 Hashes/02 Access Hashes/accessHashes2.rb
@@ -1,11 +1,11 @@
 #! /usr/local/bin/ruby
 
 
-string1 = Hash[:name, "A", :type, "info", :status, "user-bujang"]
-string2 = Hash[1 => "ABC", 2 => "abc", 3 => "Hello"]
+string1 = Hash[:name, "BUJANG", :type, "info", :status, "user-bujang"]
+string2 = Hash[1 => "Dadar Kriwil", 2 => "Kerak Telor", 3 => "PASTEL", 4 => "Emping", 5 => "batagor", 6 => "TERANG BULAN", 7 => "Lumpia", 8 => "Ongol-Ongol", 9 => "lapis legit", 10 => "URAP" ]
 
 # CHECK VALUE
-print string2.has_value? "ABC"
+print string2.has_value? "Ongol-Ongol"
 print "\n"
 
 # CHECK INDEX
@@ -13,5 +13,5 @@ print string2.select {|a, b| a > 0}
 print "\n"
 
 # ACCESS VALUE
-print "#{string2[1]}"
+print "#{string2[2]}"
 print "\n"
