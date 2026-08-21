2ac0c3996f2d4844496c7f5507a46c75393b3c9d
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 19:38:50 +0700

    Update replaceHashes3.rb

--- 08 Hashes/03 Insert and Replace/replaceHashes3.rb
+++ 08 Hashes/03 Insert and Replace/replaceHashes3.rb
@@ -4,7 +4,9 @@ hashes = Hash[1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "91735
 
 # new_hashes = {"MENDUT" => 10}
 new_hashes = {10 => "MENDUT"}
+# new_hashes = {1 => "715184725260", 2 => "Ongol-Ongol", 3 => "5785685171", 4 => "URAP", 5 => "79926869"}
 hashes.replace(new_hashes)
 
+# ACCESS ALL HASHES
 print hashes
 print "\n"
