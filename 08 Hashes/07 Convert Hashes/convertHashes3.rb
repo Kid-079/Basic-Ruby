76477a0aff3e6b42b1149ddaf6d1d22b4c9d04ab
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 20:15:00 +0700

    Update convertHashes3.rb

--- 08 Hashes/07 Convert Hashes/convertHashes3.rb
+++ 08 Hashes/07 Convert Hashes/convertHashes3.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-array = [[1, "ABC"], [2, "abc"], [3, "Abc"], [4, "12345"], [5, "123"]]
+array = [[1, "TERANG BULAN"], [2, "566575803352"], [3, "Batagor"], [4, "244877568214"], [5, "Kerak Telor"], [6, "8433875255"], [7, "urap"], [8, "715184725260"], [9, "Ongol-Ongol"], [10, "76487884794357"]]
 
 hashes = array.to_h  # Convert Array to Hashes
 
