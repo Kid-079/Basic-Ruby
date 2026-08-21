4fcdfab4d33fa3baddda108f821f22a5d70383df
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 20:10:38 +0700

    Update convertHashes2.rb

--- 08 Hashes/07 Convert Hashes/convertHashes2.rb
+++ 08 Hashes/07 Convert Hashes/convertHashes2.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-hashes = {1 => "ABC", 2 => "abc", 3 => "Abc", 4 => "12345", 5 => "123"}
+hashes = {1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "917350697854", 5 => "76487884794357", 6 => "TERANG BULAN", 7 => "715184725260", 8 => "Ongol-Ongol", 9 => "URAP", 10 => "566575803352"}
 
 data1 = hashes.to_s     # Convert to String 
 data2 = hashes.to_hash  # Convert to Hash 
