3843b5887549444f784b5f9bd773c0716b2a3928
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 20:35:15 +0700

    Update createXml3-FAILED.rb

--- 10 Experience Ruby/02 Process XML/01 Create XML/createXml3-FAILED.rb
+++ 10 Experience Ruby/02 Process XML/01 Create XML/createXml3-FAILED.rb
@@ -6,9 +6,9 @@
 
 # document = XmlMarkup.new(:target => $stdout, :indent => 1)
 # document.instruct!
-# document.comment! "This is XML"
+# document.comment! "Life Is A Journey, Enjoy The Ride"
 
 # document.name(:lang => "en") {
-# 	document.first "name_A", :info => "true"
-# 	document.last "B"
+# 	document.first "Write__Name_First", :info => "true"
+# 	document.last "Write__Name_Last"
 # }
