5dd73cecbea4286d8ba4b0050eff7c75ec4dc74a
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 19:50:37 +0700

    Update mergeHashes1.rb

--- 08 Hashes/04 Merge Hashes/mergeHashes1.rb
+++ 08 Hashes/04 Merge Hashes/mergeHashes1.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-string1 = Hash[1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "917350697854", 5 => "76487884794357", 6 => "TERANG BULAN", 7 => "715184725260", 8 => "Ongol-Ongol", 9 => "URAP", 10 => "566575803352]
+string1 = Hash[1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "917350697854", 5 => "76487884794357", 6 => "TERANG BULAN", 7 => "715184725260", 8 => "Ongol-Ongol", 9 => "URAP", 10 => "566575803352"]
 string2 = Hash[1 => "Dadar Kriwil", 2 => "79926869", 3 => "Emping", 4 => "244877568214", 5 => "Lumpia", 6 => "715184725260", 7 => "PASTEL", 8 => "5785685171", 9 => "8433875255", 10 => "Ongol-Ongol"]
 
 string1.merge string2
