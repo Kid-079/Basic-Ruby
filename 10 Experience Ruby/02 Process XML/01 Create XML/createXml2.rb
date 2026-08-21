1c7c15814510f6484d15a83a8b4e559094ff1233
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 20:38:19 +0700

    Update createXml2.rb

--- 10 Experience Ruby/02 Process XML/01 Create XML/createXml2.rb
+++ 10 Experience Ruby/02 Process XML/01 Create XML/createXml2.rb
@@ -7,15 +7,15 @@ data = Document.new
 
 data << XMLDecl.new("1.0", "UTF-8")
 
-data << Comment.new("This is XML file")
+data << Comment.new("Do The Best And Forget The Rest")
 
 data.add_element("name", {"lang" => "en"})
 
 first = data.root.add_element("first")
 last = data.root.add_element("last")
 
-first.text = "A"
-last.text = "B"
+first.text = "Journey"
+last.text = "Dreams"
 
 data.write($stdout, 0)
 print "\n"
