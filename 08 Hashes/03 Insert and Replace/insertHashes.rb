c407f5d4ece1ad288aadd0166d1fccbb2b1e5b3f
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-21 18:43:06 +0700

    Update insertHashes.rb

--- 08 Hashes/03 Insert and Replace/insertHashes.rb
+++ 08 Hashes/03 Insert and Replace/insertHashes.rb
@@ -1,6 +1,6 @@
 #! /usr/local/bin/ruby
 
-string = {1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "917350697854", 5 => "76487884794357", 6 => "TERANG BULAN", 7 => "715184725260", 8 => "Ongol-Ongol", 9 => "lapis legit", 10 => "566575803352}
+string = {1 => "Kerak Telor", 2 => "5785685171", 3 => "batagor", 4 => "917350697854", 5 => "76487884794357", 6 => "TERANG BULAN", 7 => "715184725260", 8 => "Ongol-Ongol", 9 => "lapis legit", 10 => "566575803352, 11 => "Emping", 12 => "79926869", 13 => "URAP", 14 => "244877568214", 15 => "Lumpia"}
 
 # REMOVE HASHES
 string.clear
@@ -10,7 +10,7 @@ print string.empty?
 print "\n"
 
 # INSERT HASHES
-string.store(10, "hello")
+string.store(16, "Dadar Kriwil")
 print string
 print "\n"
 print string.empty?
